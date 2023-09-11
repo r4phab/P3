f26d2b504aab03eb18c7a16f30c52030cbe5dcd0
Raphaël <r4phab@gmail.com>
2023-09-11 08:23:35 +0200

    Ajout progressBar

--- app/src/main/java/com/openclassrooms/myrepo/ui/TaskRecyclerViewAdapter.java
+++ app/src/main/java/com/openclassrooms/myrepo/ui/TaskRecyclerViewAdapter.java
@@ -8,13 +8,20 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.openclassrooms.myrepo.R;
 import com.openclassrooms.myrepo.model.Task;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
- * Un adaptateur pour afficher la liste de tâches dans un RecyclerView.
+ * Adaptateur personnalisé pour afficher la liste de tâches dans un RecyclerView.
  */
 public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewAdapter.ViewHolder> {
+
 
     /**
      * Constructeur de l'adaptateur.
@@ -23,6 +30,7 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
         super(new ItemCallback());
     }
 
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -30,17 +38,23 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
         return new ViewHolder(itemView);
     }
 
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bind(getItem(position));
     }
 
+
     /**
      * ViewHolder pour afficher les éléments de la liste de tâches.
      */
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
 
         private final TextView factTextView;
+        private final TextView dueTimeTextView;
+        private final LinearProgressIndicator progressIndicator;
+
 
         /**
          * Constructeur du ViewHolder.
@@ -48,7 +62,10 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             factTextView = itemView.findViewById(R.id.task_description);
+            dueTimeTextView = itemView.findViewById(R.id.task_duetime);
+            progressIndicator = itemView.findViewById(R.id.progress_horizontal);
         }
+
 
         /**
          * Lie les données de la tâche au ViewHolder.
@@ -57,8 +74,45 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
          */
         public void bind(Task task) {
             factTextView.setText(task.getDescription());
+
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            String formattedDueTime = dateFormat.format(task.getDueTime());
+            dueTimeTextView.setText("Date limite : " + formattedDueTime);
+
+
+            int progress = calculateProgress(task.getDueTime());
+            progressIndicator.setProgress(progress);
+        }
+
+
+        /**
+         * Calcule la valeur de progression en pourcentage pour la barre de progression.
+         *
+         * @param dueTime La date d'échéance de la tâche.
+         * @return La valeur de progression en pourcentage.
+         */
+        private int calculateProgress(Date dueTime) {
+            Calendar calendarToday = Calendar.getInstance();
+            calendarToday.set(Calendar.HOUR_OF_DAY, 0);
+            calendarToday.set(Calendar.MINUTE, 0);
+            calendarToday.set(Calendar.SECOND, 0);
+            calendarToday.set(Calendar.MILLISECOND, 0);
+
+
+            Calendar calendarDueTime = Calendar.getInstance();
+            calendarDueTime.setTime(dueTime);
+            calendarDueTime.set(Calendar.HOUR_OF_DAY, 0);
+            calendarDueTime.set(Calendar.MINUTE, 0);
+            calendarDueTime.set(Calendar.SECOND, 0);
+            calendarDueTime.set(Calendar.MILLISECOND, 0);
+
+
+            int daysDifference = (int) ((calendarDueTime.getTimeInMillis() - calendarToday.getTimeInMillis()) / (24 * 60 * 60 * 1000));
+            return 100 - (daysDifference * 10);
         }
     }
+
 
     /**
      * Callback pour la comparaison des éléments de la liste.
@@ -68,6 +122,7 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
         public boolean areItemsTheSame(@NonNull Task oldItem, @NonNull Task newItem) {
             return oldItem.getDescription().equals(newItem.getDescription());
         }
+
 
         @Override
         public boolean areContentsTheSame(@NonNull Task oldItem, @NonNull Task newItem) {
